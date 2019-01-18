b8fe39a9d0839ad9ba936c611e5b698b5a22a636
Mary Kravets <sontsevolosa@gmail.com>
2019-01-18 16:01:20 +0200

    Add some tests for application

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -15,9 +15,15 @@ module('Acceptance | list rentals', function(hooks) {
   });
 
   test('should link to information about the company.', async function (assert) {
+    await visit('/');
+    await click(".menu-about");
+    assert.equal(currentURL(), '/about', 'should navigate to about');
   });
 
   test('should link to contact information.', async function (assert) {
+    await visit('/');
+    await click(".menu-contact");
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
   });
 
   test('should list available rentals.', async function (assert) {
